1a580e137bcb24523af24a8d9aa60e67abef25c3
Josue <rjosue.ruizc@gmail.com>
2013-03-13 02:49:37 -0600

    subete

--- db/schema.rb
+++ db/schema.rb
@@ -1,99 +1,3 @@
-<<<<<<< HEAD
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended to check this file into your version control system.
-
-ActiveRecord::Schema.define(:version => 20130218215915) do
-
-  create_table "applications", :force => true do |t|
-    t.string   "name"
-    t.string   "short_description"
-    t.string   "video_url"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
-  end
-
-  create_table "applications_users", :id => false, :force => true do |t|
-    t.integer "user_id"
-    t.integer "application_id"
-  end
-
-  create_table "colleges", :force => true do |t|
-    t.string   "name"
-    t.string   "address"
-    t.string   "telefono"
-    t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "comments", :force => true do |t|
-    t.string   "text"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "faculties", :force => true do |t|
-    t.string   "name"
-    t.integer  "idcollege"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "user_profiles", :force => true do |t|
-    t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.integer  "age"
-    t.string   "address"
-    t.string   "email"
-    t.integer  "semester"
-    t.string   "phone"
-    t.string   "extra_phone"
-    t.string   "twitter"
-    t.string   "linkedin"
-    t.string   "photo"
-    t.integer  "motivation"
-    t.integer  "faculty_id"
-    t.integer  "user_profile_id"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "photo_file_name"
-    t.integer  "photo_content_type"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "authentication_token"
-    t.string   "invitation_token"
-    t.string   "password"
-    t.datetime "remember_created_at"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-end
-=======
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -188,9 +92,9 @@ ActiveRecord::Schema.define(:version => 20130218215921) do
     t.string   "photo"
     t.integer  "motivation"
     t.integer  "faculty_id"
+    t.integer  "user_profile_id"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.integer  "user_profile_id"
     t.string   "photo_file_name"
     t.integer  "photo_content_type"
     t.integer  "photo_file_size"
@@ -213,4 +117,3 @@ ActiveRecord::Schema.define(:version => 20130218215921) do
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
->>>>>>> 8ffe0ae76a384379617e6d4c380db89a893ebb0e
