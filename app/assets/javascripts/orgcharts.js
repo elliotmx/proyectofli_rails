4935d709feed669af202488937cafe0d3763b36f
Raul Contreras <raul@itechqro.com>
2013-04-19 00:00:16 -0500

    Generacion de 3 arboles de problema

--- app/assets/javascripts/orgcharts.js
+++ app/assets/javascripts/orgcharts.js
@@ -1,37 +1,41 @@
 $(document).ready(function(){
 
-$("#botonGraficas").click(function(){ 
+$(".btnDiagramas").click(function(e){ 
 
+
+    var idElemento = $(e.currentTarget).attr("rel");
+
+    //diagramaCausas
+    //diagramaConsecuencias
+    //diagramaObjetivos
 
     var options = new primitives.orgdiagram.Config(); 
 
     var rootItem = new primitives.orgdiagram.ItemConfig(); 
-    rootItem.title = "Scott Aasrud"; 
+    rootItem.title = "Problema"; 
     rootItem.description = $("#7").val(); 
     rootItem.image = null;
 
     var itemB = new primitives.orgdiagram.ItemConfig(); 
-    itemB.title = "Ted Lucas"; 
-    itemB.description = "VP, Human Resources"; 
-    itemB.image = "http://www.basicprimitives.com/demo/images/photos/b.png"; 
+    itemB.title = "Causa"; 
+    itemB.description = "Da clic para modificarme"; 
     rootItem.items.push(itemB); 
 
     var itemC = new primitives.orgdiagram.ItemConfig(); 
-    itemC.title = "Joao Stuger"; 
-    itemC.description = "Business Solutions, US"; 
-    itemC.image = "http://www.basicprimitives.com/demo/images/photos/c.png"; 
+    itemC.title = "Causa"; 
+    itemC.description = "Da clic para modificarme"; 
     rootItem.items.push(itemC); 
 
     var itemD = new primitives.orgdiagram.ItemConfig(); 
-    itemD.title = "Lynne Rathinam"; 
-    itemD.description = "GM, Enterprise Services"; 
-    itemD.image = "http://www.basicprimitives.com/demo/images/photos/d.png"; 
+    itemD.title = "Causa"; 
+    itemD.description = "Da clic para modificarme"; 
     rootItem.items.push(itemD);   
   
     var buttons = []; 
     
     buttons.push(new primitives.orgdiagram.ButtonConfig("add", "ui-icon-person", "Add")); 
-    buttons.push(new primitives.orgdiagram.ButtonConfig("delete", "ui-icon-close", "Delete")); 
+    buttons.push(new primitives.orgdiagram.ButtonConfig("delete", "ui-icon-close", "Delete"));
+    buttons.push(new primitives.orgdiagram.ButtonConfig("properties", "ui-icon-gear", "Info"))
 
     options.rootItem = rootItem; 
     options.cursorItem = rootItem; 
@@ -39,6 +43,7 @@ $("#botonGraficas").click(function(){
     options.hasButtons = primitives.common.Enabled.Auto;   
     options.hasSelectorCheckbox = primitives.common.Enabled.False; 
     options.leavesPlacementType = primitives.orgdiagram.ChildrenPlacementType.Matrix; 
+    options.onMouseClick = function(e, data){}
 
     options.onButtonClick = function(e, data) 
     { 
@@ -53,24 +58,36 @@ $("#botonGraficas").click(function(){
             { 
                 var position = primitives.common.indexOf(data.parentItem.items, data.context); 
                 data.parentItem.items.splice(position, 1); 
-                jQuery("#diagramaCausas").orgDiagram("update", primitives.orgdiagram.UpdateMode.Refresh); 
+                $("#"+idElemento).orgDiagram("update", primitives.orgdiagram.UpdateMode.Refresh); 
 
             } 
             break; 
             
-        case "add": 
-            var itemZ = new primitives.orgdiagram.ItemConfig(); 
-            itemZ .title = "Z Title"; 
-            itemZ .description = "Z Description"; 
-            itemZ .image = "http://www.basicprimitives.com/demo/images/photos/z.png"; 
-            data.context.items.push(itemZ); 
-            
-            jQuery("#diagramaCausas").orgDiagram("update", primitives.orgdiagram.UpdateMode.Refresh); 
+            case "add": 
+                var itemZ = new primitives.orgdiagram.ItemConfig(); 
+                itemZ .title = "Causa"; 
+                var newInput = prompt("¿Qué crees que es la causa de :?");
+                if(newInput!= null && newInput!=""){
+                    itemZ .description = newInput;
+                    data.context.items.push(itemZ);
+                    $("#"+idElemento).orgDiagram("update", primitives.orgdiagram.UpdateMode.Refresh);      
+                }
+
+                
+            break; 
+
+            case "properties": 
+                    var input=prompt("Modifica el Texto");
+                    if (input!=null && input!="")
+                            {data.context.description = input;}
+                    $("#"+idElemento).orgDiagram("update", primitives.orgdiagram.UpdateMode.Refresh); 
             break; 
     } 
 }; 
 
-jQuery("#diagramaCausas").orgDiagram(options);
+jQuery("#"+idElemento).orgDiagram(options);
+
+
 }); 
 
 });
