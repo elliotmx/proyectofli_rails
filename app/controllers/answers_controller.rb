309a99d572c7844cbb241178b9571fb9589c5b9f
Raul Contreras <raul@itechqro.com>
2013-04-24 01:17:00 -0500

    Se corrige problema con update en answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -24,16 +24,24 @@ def create
     #puts params.inspect
     params.each do |i,v|
       #if i!="utf8" || i!="authencity_token"
+
       if (i != "utf8") && (i != "authenticity_token") &&  (i != "commit") && (i != "action") && (i != "controller")
         puts "parametro #{i} texto: #{v}"
 
-        @answer = Answer.where(:application_id =>current_user.applications.first.id, :question_id => i.to_i ).first_or_create! do |ans|
-            ans.value = v.to_s
-            puts " dentro de update answe #{ans.inspect}"
+        @answer = Answer.where(:application_id =>current_user.applications.first.id, :question_id => i.to_i ).first
+
+        if(@answer == nil)
+            Answer.create(:application_id =>current_user.applications.first.id, :question_id => i.to_i, :value => v)
+        else
+            @answer.update_attributes(:value => v)
         end
 
+        #==begin
+        #@answer = Answer.where(:application_id =>current_user.applications.first.id, :question_id => i.to_i ).first_or_create! do |ans|
+        #    ans.value = v.to_s
+        #    puts " dentro de update answe #{ans.inspect}"
+        #end      
         puts "la variable #{@answer.inspect}"
-
       end
     	
     end
