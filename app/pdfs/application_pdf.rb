bf5567b313e0c4b6ec63f46a0f446cfa3b886e94
Raúl Contreras <rxco.ar@gmail.com>
2013-07-15 16:48:51 -0500

    Subiendo Controller PDF

---
+++ app/pdfs/application_pdf.rb
@@ -0,0 +1,24 @@
+class ApplicationPdf < Prawn::Document
+
+ def initialize(application, view)
+   super()
+   p = ""
+   r = ""
+   t = ""
+   @view = view
+   @application = application
+   @answers = Answer.where(application_id = @application.id)
+
+@answers.each do |answer|
+     question = Question.find(answer.question_id)
+     p = p+"\n\n #{question.spa_description}"
+     r = r+"\n\n #{answer.value}"
+
+     t = t + p + r
+
+     p = ""
+     r = ""
+end
+   text t
+ end
+end
