6227849dda34331707e0b9103724a119a72ec3b4
Raúl Contreras <rxco.ar@gmail.com>
2013-07-30 15:52:16 -0500

    Corrigiendo archivo de migraciones

--- db/migrate/20130730201200_change_type_of_date_position_question.rb
+++ db/migrate/20130730201200_change_type_of_date_position_question.rb
@@ -4,7 +4,7 @@ class ChangeTypeOfDatePositionQuestion < ActiveRecord::Migration
     add_column :questions, :position_question, :integer
 
     Question.reset_column_information
-    Question.find(:all).each { |question| question.update_attribute(:position_question, contact.position_question_tmp) }
+    Question.find(:all).each { |question| question.update_attribute(:position_question, question.position_question_tmp.to_i) }
     remove_column :questions, :position_question_tmp
   end
 end
