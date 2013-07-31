0a74830da84b8bbe3a68f9914ca81c8aad813934
Raúl Contreras <rxco.ar@gmail.com>
2013-07-31 02:26:51 -0500

    Se envia nuevo diagrama para actividades y tiempos

--- db/migrate/20130730201200_change_type_of_date_position_question.rb
+++ db/migrate/20130730201200_change_type_of_date_position_question.rb
@@ -1,5 +1,7 @@
 class ChangeTypeOfDatePositionQuestion < ActiveRecord::Migration
   def change
+  	#este desmadre se hizo ya que Postgree ( DB nativa de heroku), no permite hacer una migración de string a integer
+  	#si necesitas hacer la migración en local es necesario usar otro codigo
     rename_column :questions, :position_question, :position_question_tmp
     add_column :questions, :position_question, :integer
 
