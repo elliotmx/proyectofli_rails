be7b7d61b1100274af5e392b06c6c5d0b3a37939
R Josue Ruiz C <rjosue.ruizc@gmail.com>
2013-08-11 23:19:40 -0500

    cambiosContenido

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130801090213) do
+ActiveRecord::Schema.define(:version => 20130808185246) do
 
   create_table "answers", :force => true do |t|
     t.string   "value"
@@ -81,13 +81,14 @@ ActiveRecord::Schema.define(:version => 20130801090213) do
     t.string   "spa_description"
     t.string   "eng_description"
     t.boolean  "active"
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
     t.integer  "section_id"
     t.string   "html_for_field"
     t.string   "field_type"
     t.string   "extra_text_desc"
-    t.integer  "position_question", :limit => 255
+    t.integer  "position_question"
+    t.string   "pop_name"
   end
 
   create_table "sections", :force => true do |t|
