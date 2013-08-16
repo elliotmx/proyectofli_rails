7825f3d9ec89ef00d2174f1d021db99bca680b4c
root <root@christian-HP-2140.(none)>
2013-08-16 05:50:12 -0500

    Otra prueba 9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Fli::Application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'gmail.com',
+    :domain               => 'baci.lindsaar.net',
     :user_name            => 'fli.noreply@gmail.com',
     :password             => '<password>',
     :authentication       => 'fli23412',
