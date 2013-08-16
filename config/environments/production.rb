be45525c571e877683d428e0a932c31b8f75e5d2
root <root@christian-HP-2140.(none)>
2013-08-16 05:52:48 -0500

    Otra prueba 10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,14 +8,14 @@ Fli::Application.configure do
 
   #defined for devise
   config.action_mailer.default_url_options = {:host => 'www.fli.mx'}
-  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => 'baci.lindsaar.net',
     :user_name            => 'fli.noreply@gmail.com',
-    :password             => '<password>',
-    :authentication       => 'fli23412',
+    :password             => 'fli23412',
+    :authentication       => 'plain',
     :enable_starttls_auto => true  
   }
   # Full error reports are disabled and caching is turned on
