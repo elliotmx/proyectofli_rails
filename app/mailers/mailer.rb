f9185100efb23ad28e041dc7b4a431efd2bc5cb7
Raúl Contreras <rxco.ar@gmail.com>
2013-11-19 03:03:39 -0600

    Enviando Correos

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -3,8 +3,12 @@ class Mailer < ActionMailer::Base
 
   def send_email(message)
     @msg = message
-    mail( :to => @msg.email,
-    :subject => 'Thanks for signing up for our amazing fli website' )
+    #@mail = 'delarosa.bdevelop@gmail.com'
+    @mail = 'rxco.ar@gmail.com'
+
+    mail( :to => @mail,
+    :subject => 'nuevo correo desde fli.mx',
+    :content_type => "text/html" )
   end
 
 end
