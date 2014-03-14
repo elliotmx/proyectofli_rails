2810789c205aa86659c5b673dd135582618c3551
R Josue Ruiz C <rjosue.ruizc@gmail.com>
2014-03-14 00:33:44 -0600

    detalles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140314033641) do
+ActiveRecord::Schema.define(:version => 20140314052817) do
 
   create_table "answers", :force => true do |t|
     t.text     "value",          :limit => 255
